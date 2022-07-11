d409881d358adea666d471efe1338662e7eca53d
dirkt68 <dirkt6868@gmail.com>
2022-07-11 08:26:56 -0500

    see README

--- clear.py
+++ clear.py
@@ -6,7 +6,7 @@ import hashlib
 def main():
 	currentDir = os.getcwd()
 	currentFolder = os.path.basename(currentDir)
-	currentHash = "b11b60c460e286a65cfcfc88dab9a7bb5f0c8b5806d57b3cd0c3e48088cc09f6" # hash of the state of the file 5/23/22
+	currentHash = "e1db64ae9dc467e3073305d341a5e5b7c089745a45e442a803b196a93552c614" # hash of the state of the file 5/23/22
 	deletedDir = False
 	allowed = False
 
@@ -20,8 +20,7 @@ def main():
 			fileHash = hashlib.sha256(bytes).hexdigest()
 
 	except EnvironmentError:
-		print("Missing")
-		print("No configuration file found, please create a \"clear_config.json\" file, specify the folder where the report builder is stored and try again")
+		print("No configuration file found, please retrieve a certified copy.")
 		os.system("pause")
 		return
 
@@ -31,16 +30,15 @@ def main():
 			break
 
 	if not allowed or fileHash != currentHash:
-		print(f"Working Directory Not Matched - \"{currentFolder}\"")
-		print(f"Can't run clean up here, unconfigured directory")
+		print(f"Working Directory Not Matched - \"{currentFolder}\" or Config Modified")
+		print(f"Can't run clean up here!")
 		os.system("pause")
 		return
 
-	print(f"Working Directory Match - {currentFolder}")
+	print(f"Working Directory Match - \"{currentFolder}\"")
 	print("Cleaning up last run...")
 
 	for root, dirs, files in os.walk(currentDir):
-		#print(dirs)
 		if dirs == [] or dirs[0][0].isdigit() or "git" in root or dirs[0] == ".git": 
 			continue
 		
@@ -48,13 +46,15 @@ def main():
 			deletedDir = True
 			dirToRemove = root + '\\' + tempDir
 			print(f"Removing folder: {os.path.basename(dirToRemove)}")
-			shutil.rmtree(dirToRemove)
+			try:
+				shutil.rmtree(dirToRemove)
+			except Exception:
+				print(f"Failed to remove folder: {os.path.basename(dirToRemove)}, check if any files in this folder are open...")	
 	
-	os.system("pause")
-
 	if not deletedDir:
 		print("No files to delete!")
-		os.system("pause")
+
+	os.system("pause")
 
 if __name__ == '__main__':
 	main()
