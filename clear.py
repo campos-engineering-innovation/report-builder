b40a9b7e258c8493db75229d9bfd0f96ee15a5dd
dirkt68 <git@thieme-csp.com>
2024-11-14 07:11:16 -0600

    updated clear script, updated RB to v7.18.1

--- clear.py
+++ clear.py
@@ -3,58 +3,64 @@ import shutil
 import json
 import hashlib
 
+
 def main():
-	currentDir = os.getcwd()
-	currentFolder = os.path.basename(currentDir)
-	currentHash = "e1db64ae9dc467e3073305d341a5e5b7c089745a45e442a803b196a93552c614" # hash of the state of the file 5/23/22
-	deletedDir = False
-	allowed = False
+    currentDir = os.getcwd()
+    currentFolder = os.path.basename(currentDir)
+    currentHash = (
+        "0ce1f9435f4daf405aadd8d971e0a7c2c21453bbf1593ef588f0a269357d8024"
+    )  # hash of the state of the file 8/20/24
+    deletedDir = False
+    allowed = False
 
-	print("Config:", end=" ")
+    print("Config:", end=" ")
 
-	try:
-		with open("clear_config.json") as f:
-			config = json.load(f)
-		with open("clear_config.json", "rb") as fHash:
-			bytes = fHash.read()
-			fileHash = hashlib.sha256(bytes).hexdigest()
+    try:
+        with open("clear_config.json") as f:
+            config = json.load(f)
+        with open("clear_config.json", "rb") as fHash:
+            bytes = fHash.read()
+            fileHash = hashlib.sha256(bytes).hexdigest()
 
-	except EnvironmentError:
-		print("No configuration file found, please retrieve a certified copy.")
-		os.system("pause")
-		return
+    except EnvironmentError:
+        print("No configuration file found, please retrieve a certified copy.")
+        os.system("pause")
+        return
 
-	for folder in config["FOLDERS"]:
-		if folder == currentFolder:
-			allowed = True
-			break
+    for folder in config["FOLDERS"]:
+        if folder == currentFolder:
+            allowed = True
+            break
 
-	if not allowed or fileHash != currentHash:
-		print(f"Working Directory Not Matched - \"{currentFolder}\" or Config Modified")
-		print(f"Can't run clean up here!")
-		os.system("pause")
-		return
+    if not allowed or fileHash != currentHash:
+        print(f'Working Directory Not Matched - "{currentFolder}" or Config Modified')
+        print("Can't run clean up here!")
+        os.system("pause")
+        return
 
-	print(f"Working Directory Match - \"{currentFolder}\"")
-	print("Cleaning up last run...")
+    print(f'Working Directory Match - "{currentFolder}"')
+    print("Cleaning up last run...")
 
-	for root, dirs, files in os.walk(currentDir):
-		if dirs == [] or dirs[0][0].isdigit() or "git" in root or dirs[0] == ".git": 
-			continue
-		
-		for tempDir in dirs:
-			deletedDir = True
-			dirToRemove = root + '\\' + tempDir
-			print(f"Removing folder: {os.path.basename(dirToRemove)}")
-			try:
-				shutil.rmtree(dirToRemove)
-			except Exception:
-				print(f"Failed to remove folder: {os.path.basename(dirToRemove)}, check if any files in this folder are open...")	
-	
-	if not deletedDir:
-		print("No files to delete!")
+    for root, dirs, files in os.walk(currentDir):
+        if dirs == [] or dirs[0][0].isdigit() or "git" in root or dirs[0] == ".git":
+            continue
 
-	os.system("pause")
+        for tempDir in dirs:
+            deletedDir = True
+            dirToRemove = root + "\\" + tempDir
+            print(f"Removing folder: {os.path.basename(dirToRemove)}")
+            try:
+                shutil.rmtree(dirToRemove)
+            except Exception:
+                print(
+                    f"Failed to remove folder: {os.path.basename(dirToRemove)}, check if any files in this folder are open..."
+                )
 
-if __name__ == '__main__':
-	main()
+    if not deletedDir:
+        print("No files to delete!")
+
+    os.system("pause")
+
+
+if __name__ == "__main__":
+    main()
